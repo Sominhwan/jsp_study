3cf2483872e655673b44f4dadac019dfff212866
Somihwan <thalsghks@naver.com>
2023-04-07 14:25:27 +0900

    쇼핑몰, 모바일 포토

--- myapp/src/main/java/shop/ProductMgr.java
+++ myapp/src/main/java/shop/ProductMgr.java
@@ -5,6 +5,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Vector;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
 public class ProductMgr {
 	private DBConnectionMgr pool;
 	public static final String SAVEFOLDER = "C:/Jsp/myapp/src/main/webapp/shop/data/";
@@ -76,7 +81,7 @@ public class ProductMgr {
 		return bean;
 	}
 
-//Product Stock Reduce (구매 -> 재고 수정)
+	//Product Stock Reduce (구매 -> 재고 수정)
 	public void reduceProduct(OrderBean order) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -100,10 +105,96 @@ public class ProductMgr {
 	// admin mode
 	
 	// Product Insert : 상품 저장
-	
+	public boolean insertProduct(HttpServletRequest req) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		boolean flag = false;
+		try {
+			MultipartRequest multi = new MultipartRequest(req, SAVEFOLDER, MAXSIZE, ENCODING,
+					new DefaultFileRenamePolicy());
+			con = pool.getConnection();
+			sql = "insert tblProduct(name,price,detail,date,stock,image)"
+				      + "values(?,?,?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, multi.getParameter("name"));
+			pstmt.setInt(2, UtilMgr.parseInt(multi, "price"));
+			pstmt.setString(3, multi.getParameter("detail"));
+			pstmt.setString(4, UtilMgr.getDay());
+			pstmt.setInt(5, UtilMgr.parseInt(multi, "stock"));
+			if(multi.getFilesystemName("image")!=null)
+				pstmt.setString(6, multi.getFilesystemName("image"));
+			else
+				pstmt.setString(6, "ready.gif");
+			if(pstmt.executeUpdate()==1) flag = true;		
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return flag;
+	 }
 	// Product Update
+	public boolean updateProduct(HttpServletRequest req) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		boolean flag = false;
+		try {
+			MultipartRequest multi = new MultipartRequest(req, SAVEFOLDER, MAXSIZE, ENCODING,
+					new DefaultFileRenamePolicy());
+			con = pool.getConnection();
+			if(multi.getFilesystemName("image")!=null) {
+				// 이미지 수정
+				sql = "update tblProduct set name=?, price=?,"
+						+ "detail=?, stock=?, image=? where no=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, multi.getParameter("name"));
+				pstmt.setInt(2, UtilMgr.parseInt(multi, "price"));
+				pstmt.setString(3, multi.getParameter("detail"));
+				pstmt.setInt(4, UtilMgr.parseInt(multi, "stock"));
+				pstmt.setString(5, multi.getFilesystemName("image"));
+				pstmt.setInt(6, UtilMgr.parseInt(multi, "no"));
+			} else {
+				// 이미지 수정 아님
+				sql = "update tblProduct set name=?, price=?,"
+						+ "detail=?, stock=? where no=?";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, multi.getParameter("name"));
+				pstmt.setInt(2, UtilMgr.parseInt(multi, "price"));
+				pstmt.setString(3, multi.getParameter("detail"));
+				pstmt.setInt(4, UtilMgr.parseInt(multi, "stock"));
+				pstmt.setInt(5, UtilMgr.parseInt(multi, "no"));	
+			}
+			if(pstmt.executeUpdate()==1) flag = true;		
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return flag;
+	 }
 	
-	// Product Delete
-	
+	// Product Delete : 상품 이미지는 삭제하지 않음
+	public boolean deleteProduct(int no) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		boolean flag = false;
+		try {
+			con = pool.getConnection();
+			sql = "delete from tblProduct where no = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, no);
+			// no 가 pk 이기 때문에 적용되는 레코드 개수가 0,1
+			if(pstmt.executeUpdate()==1)
+				flag=true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return flag;
+	}
 	
 }
